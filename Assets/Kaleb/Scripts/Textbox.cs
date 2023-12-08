a56009c9d11a76d13c3f74330b2610428f52f876
Scott <1717922@mukilteo.wednet.edu>
2023-12-08 10:01:50 -0800

    I did the text boxes!

--- Assets/Kaleb/Scripts/Textbox.cs
+++ Assets/Kaleb/Scripts/Textbox.cs
@@ -2,51 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
 
-public class Textbox : MonoBehaviour
+public class TextBox : MonoBehaviour
 {
-    [Header("Prefabs")]
-    [SerializeField] GameObject TextandNameandImage;
-    Textbox ourTextNameFace;
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
 
-    [Header("References")]
-    TextMeshProUGUI boxText;
-    TextMeshProUGUI boxName;
-    RawImage boxFace;
-    RawImage boxImage;
-    RectTransform boxRectTransform;
-    Textbox instance;
-    Canvas Canvas;
-
-    [Header("Textbox")]
-    [SerializeField] Texture2D texture;
-
-    [Header("Typing")]
-    [SerializeField] float delayBeforeSpeaking;
-    [SerializeField] float delayAfterSpeaking = 0;
-    [SerializeField] float charDelay = 1;
-    [SerializeField] float basecharDelay =1;
-    public bool isSpeaking;
-
-    private void Awake()
+    private int index;
+    void Start()
     {
-        instance = this;
-        Textbox.ourbox = instance;
-        boxText = transform.Find("Text").gameObject.GetComponent<TextMeshProUGUI>();
-        boxName = transform.Find("Name").gameObject.GetComponent<TextMeshProUGUI>();
-        boxFace = transform.Find("Face").gameObject.GetComponent<RawImage>();
-
-        ourTextNameFace = TextandNameandImage.GetComponentInChildren<Textbox>();
-        Canvas = FindObjectOfType<Canvas>();
-
-        boxImage = GetComponent<RawImage>();
-        boxRectTransform = GetComponent<RectTransform>();
-        boxRectTransform.offsetMin = Vector2.zero();
-        boxRectTransform.offsetMax = Vector2.zero();
-        ChangeBoxImage(texture);
-
-        boxText.maxVisibleCharacters = 0;
-        boxName.maxVisibleCharacters = 0;
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
