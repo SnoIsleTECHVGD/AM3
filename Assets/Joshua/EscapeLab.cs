b1e0513abeb055e28f56d1a0a0ccb1cb3b69a8be
Rivers <1600163@mukilteo.wednet.edu>
2023-12-12 09:40:16 -0800

    Updated Layout

---
+++ Assets/Joshua/EscapeLab.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeLab : MonoBehaviour
+{
+    public GameObject winText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        winText.SetActive(true);
+    }
+}
